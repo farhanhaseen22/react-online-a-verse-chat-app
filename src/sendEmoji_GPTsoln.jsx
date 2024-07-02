5fc9247b69de3d9476fabe110ebec462fb3eeede
Farhan Reaz <farhan.haseen.22@gmail.com>
2024-07-01 22:30:19 -0230

    working on send-emojis module pt.2

---
+++ src/sendEmoji_GPTsoln.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+
+const SendEmoji = ({ onEmojiSelect }) => {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+
+  const handleEmojiSelect = (emoji) => {
+    onEmojiSelect(emoji.native);
+    setPickerVisible(false);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <button onClick={() => setPickerVisible(!isPickerVisible)}>😊</button>
+      {isPickerVisible && (
+        <div style={{ position: "absolute", bottom: "50px" }}>
+          <Picker onSelect={handleEmojiSelect} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SendEmoji;
